a8451878f35818a772f51d27825376d3caa0ae69
sauliusAbr <43952649+sauliusAbr@users.noreply.github.com>
2018-10-18 13:03:19 +0300

    Add files via upload

---
+++ Module 1.1 (1).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# task 1
+# displaying custom message
+print("Hello BV")
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+# task 2
+# inserting a new cell using Insert option; displaying message
+print("after edit, save!")
+
+
+# In[6]:
+
+
+# insert a cell, print a message showing how to save
+print("Save using shortcut Ctrl+S")
+
