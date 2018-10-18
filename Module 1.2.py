aacea4ae8c0bb458116d9e7baa33c8a44e0bc44d
sauliusAbr <43952649+sauliusAbr@users.noreply.github.com>
2018-10-18 13:10:30 +0300

    Add files via upload

---
+++ Module 1.2.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Task 1
+
+# In[1]:
+
+
+# task 1
+# [ ] enter a string in the print() function using single quotes
+
+# [ ] enter a string in the print() function using double quotes
+print('Hello world single')
+print("hello world double")
+
+
+# Task 2
+
+# In[2]:
+
+
+#task 2
+# [ ] print an Integer
+
+print(2018)
+# [ ] print a strings made of Integer characters
+print("2018")
+
+
+# Task 3
+
+# In[4]:
+
+
+# task 3
+# [ ] Review code and Run
+# initialize the variable
+current_msg = "I am a string"
+
+# print literal string
+print("I am a string")
+
+# print variable string
+print(current_msg)
+
+
+# In[5]:
+
+
+# [ ] Review code and Run
+# assign a new string to the current_msg
+current_msg = "Run this cell using Ctrl+Enter"
+
+print(current_msg)
+
+
+# In[6]:
+
+
+# { ] run cell above then run this cell after completing the code as directed
+print(current_msg)
+current_msg = 'new msg'
+print(current_msg)
+
+
+# Task 4
+
+# In[7]:
+
+
+# [ ] assign a string value to a variable student_name
+student_name = "Saulius"
+
+# [ ] print the value of variable student_name
+
+print(student_name)
+
+
+# In[8]:
+
+
+# [ ] assign the student_name variable  a different string value (a different name)
+student_name = "Paulius"
+
+# [ ] print the value of variable student_name
+print(student_name)
+
+# [ ] assign a 3rd different string value, to the variable name 
+student_name = "Simas"
+# [ ] print the value of variable name
+print(student_name)
+
+
+# In[9]:
+
+
+# [ ] assigning a value to a variable called bucket
+bucket = "kibiras"
+# [ ] print the value of bucket 
+print(bucket)
+# [ ] assign an Integer value (no quotes) to the variable bucket
+bucket = 1
+# [ ] print the value of bucket 
+print(bucket)
+
+
+# In[10]:
+
+
+# [ ] print integer 123 number
+print(123)
+# [ ] print string "123" number
+print("123")
+
+
+# In[ ]:
+
+
+
+
